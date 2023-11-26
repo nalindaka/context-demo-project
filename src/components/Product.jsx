f8b7888430f963bfdde69fad53a4ac0dc446d01d
Nalindaka Hettiarachchi <nalindakasandeep@gmail.com>
2023-11-26 11:02:34 +1000

    Move entire project to use context

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { CartContext } from "../store/shopping-cart-context";
 
 export default function Product({ id, image, title, price, description }) {
-  const { addItemtoCart } = useContext(CartContext);
+  const { addItemToCart } = useContext(CartContext);
   return (
     <article className="product">
       <img src={image} alt={title} />
@@ -13,7 +13,7 @@ export default function Product({ id, image, title, price, description }) {
           <p>{description}</p>
         </div>
         <p className="product-actions">
-          <button onClick={() => addItemtoCart(id)}>Add to Cart</button>
+          <button onClick={() => addItemToCart(id)}>Add to Cart</button>
         </p>
       </div>
     </article>
